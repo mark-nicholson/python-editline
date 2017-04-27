f5116c45548ceee4614e9bb40afbd62afc8128fb
Mark Nicholson <nicholson.mark@gmail.com>
2017-04-27 14:58:55 -0700

    Initial cut at some basic unit-tests    - really basic at this point    - starting with some base code from test_readline.py
    Signed-off-by: Mark Nicholson <nicholson.mark@gmail.com>

---
+++ test/test_editline.py
@@ -0,0 +1,99 @@
+"""
+Very minimal unittests for parts of the editline module.
+"""
+from contextlib import ExitStack
+from errno import EIO
+import os
+import selectors
+import subprocess
+import sys
+import tempfile
+import unittest
+from test.support import import_module, unlink, TESTFN
+from test.support.script_helper import assert_python_ok
+
+
+class TestEditline(unittest.TestCase):
+
+    def test_import_1(self):
+        _editline = import_module('_editline')
+
+    def test_import_2(self):
+        editline = import_module('editline')
+
+    def test_build_instance(self):
+        editline = import_module('editline')
+        el = editline.editline("testcase",
+                               sys.stdin, sys.stdout, sys.stderr)
+        self.assertIsNotNone(el)
+        
+
+class TestEditlineConsole(unittest.TestCase):
+
+    def test_import(self):
+        rc, stdout, stderr = assert_python_ok('-c', 'import editline')
+        self.assertEqual(stdout, b'')
+        self.assertEqual(rc, 0)
+    
+    def test_init(self):
+        # Issue #19884: Ensure that the ANSI sequence "\033[1034h" is not
+        # written into stdout when the readline module is imported and stdout
+        # is redirected to a pipe.
+        rc, stdout, stderr = assert_python_ok('-c', 'import editline',
+                                              TERM='xterm-256color')
+        self.assertEqual(stdout, b'')
+        self.assertEqual(rc, 0)
+
+
+def run_pty(script, input=b"dummy input\r"):
+    pty = import_module('pty')
+    output = bytearray()
+    [master, slave] = pty.openpty()
+    args = (sys.executable, '-c', script)
+    proc = subprocess.Popen(args, stdin=slave, stdout=slave, stderr=slave)
+    os.close(slave)
+    with ExitStack() as cleanup:
+        cleanup.enter_context(proc)
+        def terminate(proc):
+            try:
+                proc.terminate()
+            except ProcessLookupError:
+                # Workaround for Open/Net BSD bug (Issue 16762)
+                pass
+        cleanup.callback(terminate, proc)
+        cleanup.callback(os.close, master)
+        # Avoid using DefaultSelector and PollSelector. Kqueue() does not
+        # work with pseudo-terminals on OS X < 10.9 (Issue 20365) and Open
+        # BSD (Issue 20667). Poll() does not work with OS X 10.6 or 10.4
+        # either (Issue 20472). Hopefully the file descriptor is low enough
+        # to use with select().
+        sel = cleanup.enter_context(selectors.SelectSelector())
+        sel.register(master, selectors.EVENT_READ | selectors.EVENT_WRITE)
+        os.set_blocking(master, False)
+        while True:
+            for [_, events] in sel.select():
+                if events & selectors.EVENT_READ:
+                    try:
+                        chunk = os.read(master, 0x10000)
+                    except OSError as err:
+                        # Linux raises EIO when slave is closed (Issue 5380)
+                        if err.errno != EIO:
+                            raise
+                        chunk = b""
+                    if not chunk:
+                        return output
+                    output.extend(chunk)
+                if events & selectors.EVENT_WRITE:
+                    try:
+                        input = input[os.write(master, input):]
+                    except OSError as err:
+                        # Apparently EIO means the slave was closed
+                        if err.errno != EIO:
+                            raise
+                        input = b""  # Stop writing
+                    if not input:
+                        sel.modify(master, selectors.EVENT_READ)
+
+
+if __name__ == "__main__":
+    unittest.main()
