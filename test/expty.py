90a9c9ff525d7fd072a9df6018ecbc83c6cd5deb
Mark Nicholson <nicholson.mark@gmail.com>
2017-05-17 21:33:46 -0700

    Code updates and documentation to satisfy pylint   - docstrings   - pep8 conformance
    Signed-off-by: Mark Nicholson <nicholson.mark@gmail.com>

--- test/expty.py
+++ test/expty.py
@@ -1,5 +1,7 @@
+"""
+Module providing support to create more interactive PTY sessions.
+"""
 
-import sys
 import os
 import time
 from errno import EIO
@@ -8,14 +10,30 @@ import subprocess
 import selectors
 
 class InteractivePTY(object):
+    """Class implementation of a PTY interface to an interactive interpreter.
+    This is used to provide tool automation in a more simplistic form.
+    """
 
     def __init__(self, tool, prompt, exit_cmd, args=[], encoding='UTF-8'):
+        """Create an interactive PTY session
+        tool     - full name of the executable to run
+        prompt   - token to treat as the interpreter's prompt
+        exit_cmd - command to tell the interpreter to exit cleanly
+        args     - extra arguments (list) to pass to launcher (def: [])
+        encoding - downstream interpreter's encoding (def: UTF-8)
+        """
+        if not os.path.exists(tool):
+            raise ValueError("'{}' is not found".format(tool))
+
+        if prompt == '':
+            raise ValueError("prompt must be a valid string")
+
         self._tool = tool
         self._tool_args = args
         self._prompt = prompt
         self._encoding = encoding
         self._exit_cmd = exit_cmd
-        
+
         # where to accumulate incoming data
         self._raw_buffer = b''
 
@@ -23,11 +41,12 @@ class InteractivePTY(object):
         self.master, self.slave = pty.openpty()
 
         # hook it up to a subprocess
-        args = [ self._tool ] + self._tool_args
+        args = [self._tool] + self._tool_args
         self.proc = subprocess.Popen(args,
                                      stdin=self.slave,
                                      stdout=self.slave,
                                      stderr=self.slave)
+        self.proc_rc = None
 
         # register a selector manager
         self.sel = selectors.SelectSelector()
@@ -36,10 +55,12 @@ class InteractivePTY(object):
 
         # try to avoid os interference
         os.set_blocking(self.master, False)
-        
+
     def __del__(self):
-        # normally, this is not necessary, but this class could have
-        # a bunch of dangling resources.
+        """Normally, this is not necessary, but this class could have
+        a bunch of dangling resources, in particular the subprocess.
+        If it is not dead, it is killed off here for sure.
+        """
         if self.proc:
             try:
                 self.proc.kill()
@@ -52,14 +73,29 @@ class InteractivePTY(object):
             os.close(self.slave)
 
     def close(self):
-        r = self.cmd(self._exit_cmd)
+        """Shut down the interpreter cleanly, waiting for it to exit.
+        This routine requires 'exit_cmd' to be set in the constructor
+        as it is issued here to try to get the interpreter to close-out
+        naturally.
+          Attribute proc_rc is set with the result of the wait.
+        """
+        self.cmd(self._exit_cmd)
         self.proc_rc = self.proc.wait()
 
     def get_data(self, marker='\n', timeout=0):
+        """Collect the output of the interpreter.
+
+        marker - token to identify the end of the output ('\n')
+        timeout - seconds to keep looking for the marker before giving up (0)
+
+        Returns tuple: (tok, predata)
+           tok     - the matched marker
+           predata - everything read up until marker was found
+        """
 
         # the data is managed as bytes
         marker = marker.encode(self._encoding)
-        
+
         # do we already have it?
         tok, predata, self._raw_buffer = self._parse_marker(marker,
                                                             self._raw_buffer)
@@ -68,7 +104,7 @@ class InteractivePTY(object):
 
         # remember when we begin
         end_time = time.time() + timeout
-        
+
         # nope, rummage for more data
         while True:
             for [_, events] in self.sel.select(timeout):
@@ -92,15 +128,31 @@ class InteractivePTY(object):
                         self._raw_buffer += chunk  #.decode(self._encoding)
 
                         # check for the marker
-                        tok, predata, self._raw_buffer = self._parse_marker(marker, self._raw_buffer)
+                        tok, predata, self._raw_buffer = \
+                            self._parse_marker(marker, self._raw_buffer)
                         if tok == marker:
-                            return tok.decode(self._encoding), predata.decode(self._encoding)
+                            return (
+                                tok.decode(self._encoding),
+                                predata.decode(self._encoding)
+                                )
                 else:
                     # bailout if the timeout has exceeded
                     if end_time < time.time():
                         return None, None
 
-    def _parse_marker(self, marker, data):
+    @staticmethod
+    def _parse_marker(marker, data):
+        """Snoop through the given data looking for the marker.
+
+        marker - token to look for
+        data - data to be acted on
+
+        Note: both arguments must match in type! (str,str) or (bytes,bytes)
+
+        Return the found marker, the data preceding it and the data following.
+        If the returned marker is None, it was not found and predata is also
+        None.
+        """
         try:
             predata, data = data.split(marker, 1)
         except ValueError:
@@ -111,8 +163,16 @@ class InteractivePTY(object):
         return marker, predata, data
 
     def send_data(self, data, add_crlf=True):
+        """Issue data to the interpreter.
+
+        data - str containing the info to send
+        add_crlf - add a 'return' to the end of the data (default: True)
+
+        It is not advised to use this externally, but it can be
+        handy in certain cases.
+        """
         retries = 5
-        
+
         # most interpreters want a newline
         if add_crlf:
             data += '\n'
@@ -144,20 +204,29 @@ class InteractivePTY(object):
                     return ''
 
     def first_prompt(self):
+        """Collect the initial start-up output generated from the interpreter
+        and return it to the user.  Searches for self._prompt as the indication
+        the interpreter is ready to go.
+        """
+
         # collect initial output
-        p,d = self.get_data(self._prompt, 1)
+        prompt, data = self.get_data(self._prompt, 1)
 
         # got it...
-        if p is not None:
-            return d
+        if prompt is not None:
+            return data
 
         # no prompt? snooze a bit and retry
         time.sleep(0.1)
-        p,d = self.get_data(self._prompt, 1)
-        return d
-        
+        prompt, data = self.get_data(self._prompt, 1)
+        return data
+
     def sync_prompt(self):
-        save_raw_buffer = self._raw_buffer
+        """Try to coax the interpreter back in to a clean state.
+
+        Returns True if it succeeds.
+        """
+
         self._raw_buffer = b''
 
         # poke it a few times...
@@ -165,21 +234,38 @@ class InteractivePTY(object):
         while tries > 0:
             tries -= 1
             self.send_data('\n')
-            p,d = self.get_data(self.prompt, 0.1)
-            if p == self.prompt:
+            (prompt, _) = self.get_data(self._prompt, 0.1)
+            if prompt == self._prompt:
                 return True
 
         # hmm. dead?
         return False
-            
 
-    def cmd(self, cmd, marker=None, as_string=False, timeout=1, trim_cmd=True, add_crlf=True):
+
+    def cmd(self, cmd, marker=None, as_string=False,
+            timeout=1, trim_cmd=True, add_crlf=True):
+        """Issue a command-string to the underlying interpreter
+        then collect and return any generated output until a marker/prompt
+        is seen.
+
+        cmd - the single-line command string to execute
+        marker - prompt or token to declare the command is done
+                  (default self._prompt)
+        as_string - prefer the response to be one big str
+        timeout - seconds of delay before declaring that the interpreter wedged
+        trim_cmd - remove the 'echo' of the command string from output
+                   (default: True)
+        add_crlf - append a CR/LF combination to complete the cmd
+                   (default: True)
+        """
+
+        # make sure a marker is set
         if marker is None:
             marker = self._prompt
 
         # clear out any accumulated cruft
         while True:
-            prompt,d = self.get_data(marker, timeout=0)
+            prompt, data = self.get_data(marker, timeout=0)
             if prompt is None:
                 break
 
@@ -203,9 +289,16 @@ class InteractivePTY(object):
         return lines
 
     def run_script(self, script):
+        """Issue a sequence of commands (script) to the interpreter, then
+        collect all generated output and return it.
+
+        script - [str|list] sequence of commands to run
+
+        Note: will split 'str' based on os.linesep.
+        """
 
         # scripts should be run one line at a time
-        if type(script) is str:
+        if isinstance(script, str):
             cmds = script.split(os.linesep)
         else:
             cmds = script
@@ -213,10 +306,10 @@ class InteractivePTY(object):
         # want to collect all the output in one go
         output = []
 
-        # iterate them 
+        # iterate them
         for cmd in cmds:
-            r = self.cmd(cmd)
-            output.extend(r)
+            cmd_output = self.cmd(cmd)
+            output.extend(cmd_output)
 
         # done
         return output
